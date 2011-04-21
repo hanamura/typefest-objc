22cfbddf2ee432368e4a8829a104f192e2287ab1
Taro Hanamura <fixatif@gmail.com>
2011-04-21 19:05:26 +0900

    Initial Commit

---
+++ typefest-objcTests/typefest_objcTests.h
@@ -0,0 +1,17 @@
+//
+//  typefest_objcTests.h
+//  typefest-objcTests
+//
+//  Created by hanamura on 11/04/21.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface typefest_objcTests : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ typefest-objcTests/typefest_objcTests.m
@@ -0,0 +1,33 @@
+//
+//  typefest_objcTests.m
+//  typefest-objcTests
+//
+//  Created by hanamura on 11/04/21.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "typefest_objcTests.h"
+
+
+@implementation typefest_objcTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in typefest-objcTests");
+}
+
+@end
